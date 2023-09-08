f18a2e72cbac45527d72a381ef67fe7a56817108
Dusko Mirkovic <dmirkovic@biomech.us>
2023-09-08 15:20:56 +0200

    CTX-4589: Remove unused/testing code

--- bio-bodysite-prediction/preprocess/visualise_info_data.py
+++
@@ -1,5 +0,0 @@
-infoFilePath = ""
-
-with open(infoFilePath, "r") as infoFile:
-    for i in range(1, 100):
-        print(infoFile.readline())
--- bio-bodysite-prediction/preprocess/visualise_mapped_data.py
+++
@@ -1,9 +0,0 @@
-mappedFilePath = "" # path to atlas .mapped file
-
-print(".MAPPED FILE: \n")
-print("Header:")
-print("Sample id, \t RNA read count, \t mapped rads, \t OTU count \n")
-
-with open(mappedFilePath, "r") as mappedFile:
-    for i in range(1, 100):
-        print(mappedFile.readline())
