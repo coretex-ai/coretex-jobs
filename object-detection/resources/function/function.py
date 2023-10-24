e0af1df1ace78b60ddbcd4590e3a05452c5eb110
Vuk Manojlovic <vmanojlovic@biomech.us>
2023-10-24 10:44:31 +0200

    CTX-4793: Added function to the object detection template

---
+++ object-detection/resources/function/function.py
@@ -0,0 +1,55 @@
+from typing import Any, Optional
+from pathlib import Path
+
+from coretex import folder_manager
+
+import detect
+
+
+def badRequest(error: str) -> dict[str, Any]:
+    return {
+        "code": 400,
+        "body": {
+            "error": error
+        }
+    }
+
+
+def success(data: Optional[Any] = None) -> dict[str, Any]:
+    if data is None:
+        data = {}
+
+    return {
+        "code": 200,
+        "body": data
+    }
+
+
+def prepareInputData(inputPath: Path) -> Optional[Path]:
+    imageDir = folder_manager.createTempFolder("images")
+
+    if isinstance(inputPath, Path) and inputPath.is_file():
+        inputPath.link_to(imageDir / inputPath.name)
+        return imageDir
+
+    return None
+
+
+def response(requestData: dict[str, Any]) -> dict[str, Any]:
+    modelDir = requestData.get("model")
+    weightsPath = modelDir / "model.pt"
+
+    imageSize = requestData.get("imageSize", 640)
+    if isinstance(imageSize, str):
+        imageSize = int(imageSize)
+
+    inputPath = prepareInputData(requestData.get("inputFile"))
+    if inputPath is None:
+        return badRequest("Invalid input data. Single image file expected")
+
+    boundingBoxes, classes = detect.run(inputPath, weights = weightsPath, imgsz = (imageSize, imageSize))
+
+    return success({
+        "bbox": boundingBoxes,
+        "class": classes
+    })
