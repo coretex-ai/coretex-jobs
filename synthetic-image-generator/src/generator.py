e6b4099bdd0d77e855ffb79b3d5b02e60dea132b
Vuk Manojlovic <vmanojlovic@biomech.us>
2023-11-16 15:12:55 +0100

    CTX-4915: Changed experiment.config to task.yaml

--- synthetic-image-generator/src/generator.py
+++ synthetic-image-generator/src/generator.py
@@ -11,13 +11,11 @@ import cv2
 
 from coretex import (
     ImageDataset, ImageSample, AnnotatedImageSampleData, ImageDatasetClasses,
-    CoretexImageAnnotation, CoretexSegmentationInstance, BBox
+    CoretexImageAnnotation, CoretexSegmentationInstance
 )
 
+from .annotations import transformAnnotation
 from .utils import Rect, uploadAugmentedImage
-
-
-SegmentationType = list[int]
 
 
 def generateSegmentedImage(
@@ -125,61 +123,6 @@ def composeImage(
         scalingModifiers.append(scalingModifier)
 
     return background, centroids, scalingModifiers
-
-
-def applyAffine(point: tuple[int, int], transformMatrix: ndarray) -> tuple[int, int]:
-    columnVector = np.array([[point[0], point[1], 1]]).T
-    hm = np.matmul(transformMatrix, columnVector).flatten()
-    return (abs(int(hm[0] / hm[2])), abs(int(hm[1] / [hm[2]])))
-
-
-def affineSegmentation(inputSegmentation: list[int], transformMatrix: np.ndarray) -> SegmentationType:
-    outputSegmentation: list[int] = []
-    for i in range(0, len(inputSegmentation), 2):
-        cornerPoint = (inputSegmentation[i], inputSegmentation[i + 1])
-        outputSegmentation.extend(applyAffine(cornerPoint, transformMatrix))
-
-    return outputSegmentation
-
-
-def transformAnnotation(
-    instance: CoretexSegmentationInstance,
-    transformMatrix: Optional[np.ndarray],
-    scale: float,
-    centroid: tuple[int, int],
-    angle: int,
-    unwarp: bool,
-    mask: ndarray
-) -> CoretexSegmentationInstance:
-
-    segmentations = instance.segmentations
-    if unwarp and transformMatrix is not None:
-        points = Rect.extractRectangle(mask).numpy().flatten()
-        segmentations = [np.append(points, [points[0], points[1]]).tolist()]
-        segmentations = [affineSegmentation(sublist, transformMatrix) for sublist in segmentations]
-
-    segmentationsScaled = [[value * scale for value in sublist] for sublist in segmentations]
-    segmentationsFlattened = [sample for sublist in segmentationsScaled for sample in sublist]
-
-    augmentedInstance = CoretexSegmentationInstance.create(
-        instance.classId,
-        BBox.fromPoly(segmentationsFlattened),
-        segmentationsScaled
-    )
-
-    augmentedInstance.rotateSegmentations(angle)
-    augmentedInstance.centerSegmentations(centroid, getCenter(augmentedInstance.segmentations))
-
-    return augmentedInstance
-
-
-def getCenter(segmentations: list[SegmentationType]) -> tuple[int, int]:
-    segmentation = segmentations[0]
-    xs = [segmentation[i] for i in range(0, len(segmentation), 2)]
-    ys = [segmentation[i + 1] for i in range(0, len(segmentation), 2)]
-    center = (0.5 * (max(xs) - min(xs)) + min(xs), 0.5 * (max(ys) - min(ys)) + min(ys))
-
-    return center
 
 
 def processInstance(
