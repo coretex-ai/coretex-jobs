681809392f17550e4766b4ddf23ec6f7e4755caf
Vuk Manojlovic <vmanojlovic@biomech.us>
2023-12-04 17:14:19 +0100

    Hotfix-OCR: Improved messaging for failed document detection

--- tasks/document-ocr-fn/resources/function/function.py
+++ tasks/document-ocr-fn/resources/function/function.py
@@ -10,6 +10,14 @@ from ocr import performOCR
 from object_detection import runObjectDetection
 
 
+DOCUMENT_NOT_FOUND = {
+    "code": 404,
+    "body": {
+        "error": "Failed to find document on the provided image"
+    }
+}
+
+
 def response(requestData: dict[str, Any]) -> dict[str, Any]:
     modelsDir = requestData.get("model")
 
@@ -23,11 +31,11 @@ def response(requestData: dict[str, Any]) -> dict[str, Any]:
     predictedMask = detect_document.run(segmentationModel, imagePath)
     predictedMask = processMask(predictedMask)
     if predictedMask is None:
-        return functions.badRequest("Failed to determine document borders")
+        return DOCUMENT_NOT_FOUND
 
     segmentedImage = segmentImage(imagePath, predictedMask)
     if segmentedImage is None:
-        return functions.badRequest("Failed to determine document borders")
+        return DOCUMENT_NOT_FOUND
 
     bboxes, classes = runObjectDetection(segmentedImage, objDetModelWeights)
     segmentedDetections = segmentDetections(segmentedImage, bboxes)
