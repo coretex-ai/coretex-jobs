03b452d917d8f1b398751f5500314f3b0ca94179
Vuk Manojlovic <vmanojlovic@biomech.us>
2024-05-21 17:20:54 +0200

    CTX-5990: Created RAG indexing and chatbot templates

---
+++ tasks/ollama-rag-chatbot-fn/main.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+import os
+import shutil
+import logging
+
+from coretex import currentTaskRun, Model, folder_manager, CustomDataset
+
+
+def copyDir(src: Path, dst: Path, directoryName: str) -> None:
+    shutil.copytree(src, dst / directoryName, copy_function = os.link)
+
+
+def getIndexPath(dataset: CustomDataset):
+    sample = dataset.samples[0]
+    sample.unzip()
+
+    return sample.path / "embeddings.index"
+
+
+def main():
+    taskRun = currentTaskRun()
+    taskRun.dataset.download()
+    sample = taskRun.dataset.samples[0]
+    sample.unzip()
+
+    model = Model.createModel(f"{taskRun.id}-rag-chatbot", taskRun.id, 1.0, {})
+
+    modelFunction = Path(".", "resources", "function")
+    resDir = folder_manager.createTempFolder("resDir")
+
+    copyDir(modelFunction, resDir, "function")
+    copyDir(sample.path, resDir, "corpus-index")
+
+    model.upload(resDir)
+
+    logging.info(">> [DocumentOCR] Model deployed \U0001F680\U0001F680\U0001F680")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tasks/ollama-rag-chatbot-fn/resources/function/utils.py
@@ -0,0 +1,20 @@
+from typing import Any
+from pathlib import Path
+
+import pickle
+import faiss
+import numpy as np
+
+
+def loadCorpusAndIndex(dirPath: Path) -> tuple[np.ndarray, Any]:
+    with open(dirPath / "corpus.pkl", "rb") as file:
+        corpus = pickle.load(file)
+
+    index = faiss.read_index(str(dirPath / "embeddings.index"))
+
+    return corpus, index
+
+
+def retrieveDocuments(queryEmbedding: np.ndarray, index: faiss.IndexFlatL2, corpus: np.ndarray, k: int):
+    distances, indices = index.search(queryEmbedding, k)
+    return [(corpus[i], distances[0][j]) for j, i in enumerate(indices[0])]
