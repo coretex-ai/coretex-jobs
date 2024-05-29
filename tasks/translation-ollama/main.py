ba4154a6844c83e7cbb244c6f06b8a43ebb4d0b9
Dusko Vesic <duskovesic13@gmail.com>
2024-05-29 16:18:37 +0200

    Task for translating pdf files using LLM

---
+++ tasks/translation-ollama/main.py
@@ -0,0 +1,83 @@
+from coretex import currentTaskRun
+from coretex import CustomDataset
+from typing import Any
+
+from pathlib import Path
+
+import logging
+import fitz
+import ollama
+import zipfile
+
+import numpy as np
+
+from model import launchOllamaServer, pullModel, LLM
+
+
+def readPDF(filePath: Path) -> list[str]:
+    pagesText: list[str] = []
+    with fitz.open(filePath) as doc:
+        for page in doc:
+            paragraphs = page.get_text().split("\n")
+            pagesText.extend(paragraphs)
+    return pagesText
+
+
+def loadCorpus(dataset: CustomDataset) -> list[list[str]]:
+    corpus: list[list[str]] = []
+    for sample in dataset.samples:
+        sample.unzip()
+
+        pdfPaths = [path for path in sample.path.iterdir() if path.suffix == ".pdf"]
+
+        for pdfPath in pdfPaths:
+            corpus.append(readPDF(pdfPath))
+
+    return corpus
+
+
+def main() -> None:
+    taskRun = currentTaskRun()
+    dataset: CustomDataset = taskRun.dataset
+    dataset.download()
+
+    launchOllamaServer()
+    
+    logging.info(">> [OllamaRAG] Pulling model")
+    pullModel(LLM)
+    
+    logging.info(">> [OllamaRAG] Loading text corpus")
+    corpus = loadCorpus(taskRun.dataset)
+    
+    newDatasetName = f"{taskRun.id}-translated"
+    newDataset = CustomDataset.createDataset(newDatasetName, taskRun.projectId)
+
+    language = taskRun.parameters["language"]
+    counter: int = 1
+    for document in corpus:
+        document = [x.strip() for x in document]
+        while "" in document:
+            document.remove("")
+
+        translatedText: str = ""
+        for paragraph in document:
+            logging.info(">> [OllamaRAG] Translating paragraph")
+            query = f"I will send you one paragraph, you translate into {language}. Let your response be only the translation of the sent paragraph, without additional comments. The paragraph to be translated is: {paragraph}"
+            msg = {
+                "role": "user",
+                "content": query
+            }
+            response = ollama.chat(model = LLM, messages = [msg])
+            answer = response["message"]["content"]
+            translatedText = translatedText + answer + "\n"
+        
+        with open(f"{counter}.txt", "w") as file:
+            file.write(translatedText)
+        with zipfile.ZipFile(f"{counter}.zip", "w") as zipFile:
+            zipFile.write(f"{counter}.txt")
+        filePath = f"{counter}.zip"
+        newDataset.add(filePath)
+        counter += 1
+
+
+main()
---
+++ tasks/translation-ollama/model.py
@@ -0,0 +1,70 @@
+import logging
+import subprocess
+
+from ollama import ResponseError
+
+import time
+import requests
+import ollama
+
+
+OLLAMA_SERVER_URL = "http://127.0.0.1:11434"
+LLM = "llama3"
+
+
+def warmup() -> None:
+    ollama.chat(
+    model = LLM,
+    messages = [{
+        "role": "user",
+        "content": f"This is a test and a warmup. Respond with \"OK\"."
+    }]
+)
+
+
+def isOllamaInstalled() -> bool:
+    try:
+        subprocess.run(["ollama", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
+
+
+def installOllama() -> None:
+    try:
+        subprocess.run("curl https://ollama.ai/install.sh | sh", shell=True, check=True)
+        logging.info(">> [OllamaRAG] Ollama installation was successful")
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f">> [OllamaRAG] An error occurred during Ollama installation: {e}")
+
+
+def checkOllamaServer() -> bool:
+    try:
+        response = requests.get(OLLAMA_SERVER_URL)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.ConnectionError:
+        return False
+
+
+def launchOllamaServer() -> subprocess.Popen[bytes]:
+    if not isOllamaInstalled():
+        installOllama()
+
+    if checkOllamaServer():
+        return None
+
+    logging.info(">> [OllamaRAG] Launching ollama sever")
+    return subprocess.Popen(["ollama", "serve"])
+
+
+def pullModel(model: str) -> None:
+    # Sleep for 1 second to give the server time to start
+    time.sleep(1)
+
+    try:
+        ollama.pull(model)
+    except ResponseError:
+        pullModel(model)
