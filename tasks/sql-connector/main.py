03666578351dde89d42e6bf39d8182854182d6ae
Dusko Vesic <duskovesic13@gmail.com>
2024-06-12 13:20:45 +0200

    code improvements

--- tasks/sql-connector/main.py
+++ tasks/sql-connector/main.py
@@ -6,12 +6,14 @@ import zipfile
 
 from mysql.connector import Error
 
-import mysql.connector
+#import mysql.connector
+import psycopg2
 
-from coretex import currentTaskRun, CustomDataset, CredentialsSecret
+from coretex import currentTaskRun, CustomDataset
 
 
 def main() -> None:
+    """
     taskRun = currentTaskRun()
     credentials = taskRun.parameters["credentials"]
     host = taskRun.parameters["host"]
@@ -25,6 +27,75 @@ def main() -> None:
         'database': database,
         'port': port
     }
+    """
+    taskRun = currentTaskRun()
+    user = taskRun.parameters["user"]
+    password = taskRun.parameters["password"]
+    host = taskRun.parameters["host"]
+    port = taskRun.parameters["port"]
+    database = taskRun.parameters["database"]
+
+    configForConnection = {
+        'user': user,
+        'password': password,
+        'host': host,
+        'database': database,
+        'port': port
+    }
+
+
+    try:
+        logging.info("Connecting with database")
+        conn = psycopg2.connect(**configForConnection)
+        #if not conn.closed():
+         #   logging.warning("ostvarena konekcija")
+
+        dataset = CustomDataset.createDataset(f"{taskRun.id}-{database}", taskRun.projectId)
+
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT table_name FROM information_schema.tables WHERE table_schema = '{database}'")
+        tables = list(cursor.fetchall())
+        tables = [table[0] for table in tables]
+        logging.warning(f"marker {cursor}")
+        for table in tables:
+            dataFromTableForCSV: list[dict] = []
+                
+            cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_schema = '{database}' AND table_name = '{table}'")
+            columnNames = list(cursor.fetchall())
+            columnNames = [name[0] for name in columnNames]
+                
+            cursor.execute(f"SELECT * FROM {table}")
+            rows = list(cursor.fetchall())
+                
+            for row in rows:
+                dataFromTableForCSV.append(dict(zip(columnNames, list(row))))
+
+            with open(f"{table}.csv", "w", newline="") as file:
+                writer = csv.DictWriter(file, fieldnames=columnNames)
+                writer.writeheader()
+                writer.writerows(dataFromTableForCSV)
+                
+            with zipfile.ZipFile(f"{table}.zip", "w") as zipFile:
+                zipFile.write(f"{table}.csv")
+
+            dataset.add(f"{table}.zip")
+
+            Path(f"{table}.csv").unlink()
+            Path(f"{table}.zip").unlink()
+
+        conn.close()
+        logging.info("Connection with database is closed")
+
+    except:
+        logging.error("Error while connecting to database")
+
+
+
+
+
+
+
+    """
     try:
         logging.info("Connecting with database")
         conn = mysql.connector.connect(**configForConnection)
@@ -68,7 +139,7 @@ def main() -> None:
 
     except Error as e:
         logging.error(f"Error while connecting to database {e}")
-
+    """
     
 
 if __name__ == "__main__":
