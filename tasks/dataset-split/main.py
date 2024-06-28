471621a7dc743d4fade5747d923c32f1dac10337
Dusko Vesic <duskovesic13@gmail.com>
2024-06-28 13:16:26 +0200

    code improvements

--- tasks/dataset-split/main.py
+++ tasks/dataset-split/main.py
@@ -1,10 +1,10 @@
 import logging
 
-from coretex import currentTaskRun, ImageDataset, CustomDataset, SequenceDataset, NetworkDataset
+from coretex import currentTaskRun, ImageDataset, CustomDataset, SequenceDataset
 
-from src.customDatasetSplit import customDatasetSplit
-from src.imageDatasetSplit import imageDatasetSplit
-from src.sequenceDatasetSplit import sequenceDatasetSplit
+from src.splitCustomDataset import splitCustomDataset
+from src.splitImageDataset import splitImageDataset
+from src.splitSequenceDataset import splitSequenceDataset
 from src.utils import DatasetType
 
 
@@ -25,22 +25,22 @@ def main() -> None:
 
     if isinstance(originalDataset, ImageDataset):
         logging.info(f">> [Dataset Split] Splitting ImageDataset {originalDataset.name}...")
-        splitDatasets = imageDatasetSplit(originalDataset, datasetParts, taskRunId, projectId)
+        splitDatasets = splitImageDataset(originalDataset, datasetParts, taskRunId, projectId)
 
     if isinstance(originalDataset, CustomDataset):
         try:
-            # "If setDatasetType(SequenceDataset) cannot be executed, it raises a FileNotFoundError,
+            # If setDatasetType(SequenceDataset) cannot be executed, it raises a FileNotFoundError,
             # and then the dataset is split as CustomDataset.
             # The difference between SequenceDataset and CustomDataset is that
-            # SequenceDataset requires a metadata file."
+            # SequenceDataset requires a metadata file.
 
             taskRun.setDatasetType(SequenceDataset)
             originalDataset = taskRun.dataset
             logging.info(f">> [Dataset Split] Splitting SequenceDataset {originalDataset.name}...")
-            splitDatasets = sequenceDatasetSplit(originalDataset, datasetParts, taskRunId, projectId)
+            splitDatasets = splitSequenceDataset(originalDataset, datasetParts, taskRunId, projectId)
         except FileNotFoundError:
             logging.info(f">> [Dataset Split] Splitting CustomDataset {originalDataset.name}...")
-            splitDatasets = customDatasetSplit(originalDataset, datasetParts, taskRunId, projectId)
+            splitDatasets = splitCustomDataset(originalDataset, datasetParts, taskRunId, projectId)
 
     outputDatasets = [dataset.id for dataset in splitDatasets]
     taskRun.submitOutput("outputDatasets", outputDatasets)
