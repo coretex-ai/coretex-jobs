57fe81abf98ed2242c7feccd73d08a61322d79ef
nemanja <nmilutinovic@biomech.us>
2024-07-17 10:09:53 +0200

    parallelism implemented

--- .github/workflows/linter-code-check.sh
+++ .github/workflows/linter-code-check.sh
@@ -1,45 +1,58 @@
 #!/bin/bash
 
 eval "$(conda shell.bash hook)"
-for dir in tasks/* ; do
-  echo "Checking directory: $dir"
+dir=$1
+echo "DIR is = $dir"
+# for dir in tasks/* ; do
+echo "Checking directory: $dir"
 
-  # Skip the directory if no .mypy.ini file is found
-  if [ ! -f "$dir/.mypy.ini" ]; then
-    echo "No .mypy.ini file found in $dir, skipping..."
-    continue
-  fi
+# Skip the tasks/dataset-split directory
+if [ "$dir" == "tasks/dataset-split/" ]; then
+  exit 0;
+fi
+# Skip the directory if no .mypy.ini file is found
+if [ ! -f "$dir/.mypy.ini" ]; then
+  echo "No .mypy.ini file found in $dir, skipping..."
+  # continue
+  exit 0
+fi
+date1=$(date +"%s")
+if [ -f "$dir/environment.yml" ]; then
+  echo "Setting up conda environment for $dir"
+  conda env create -n $(basename "$dir") -f "$dir/environment.yml"
+  echo "Created conda environment"
+  conda activate $(basename "$dir")
+  pip install mypy
+elif [ -f "$dir/requirements.txt" ]; then
+  echo "Setting up venv for $dir"
+  python3.9 -m venv "$dir/venv"
+  echo "activate venv"
+  source "$dir/venv/bin/activate"
+  echo "install requirements"
+  pip install --upgrade pip
+  pip install -r "$dir/requirements.txt"
+  pip install mypy
+fi
 
-  if [ -f "$dir/environment.yml" ]; then
-    echo "Setting up conda environment for $dir"
-    conda env create -n $(basename "$dir") -f "$dir/environment.yml"
-    echo "Created conda environment"
-    conda activate $(basename "$dir")
-    pip install mypy
-  elif [ -f "$dir/requirements.txt" ]; then
-    echo "Setting up venv for $dir"
-    python3.9 -m venv "$dir/venv"
-    source "$dir/venv/bin/activate"
-    pip install -r "$dir/requirements.txt"
-    pip install mypy
-  fi
+echo "Running mypy in $dir"
+set +e  # Disable exit on error
+mypy_output=$(mypy --config-file "$dir/.mypy.ini" "$dir" 2>&1)
+set -e  # Re-enable exit on error
 
-  echo "Running mypy in $dir"
-  set +e  # Disable exit on error
-  mypy_output=$(mypy --config-file "$dir/.mypy.ini" "$dir" 2>&1)
-  set -e  # Re-enable exit on error
+echo "$mypy_output"
+if echo "$mypy_output" | grep -q 'error:'; then
+  echo "Running install-types in $dir"
+  mypy --install-types --non-interactive --config-file "$dir/.mypy.ini" "$dir"
+fi
 
-  echo "$mypy_output"
-  if echo "$mypy_output" | grep -q 'error:'; then
-    echo "Running install-types in $dir"
-    mypy --install-types --non-interactive --config-file "$dir/.mypy.ini" "$dir"
-  fi
-
-  if [ -f "$dir/environment.yml" ]; then
-    conda deactivate
-    conda remove -y -n $(basename "$dir") --all
-  elif [ -f "$dir/requirements.txt" ]; then
-    deactivate
-    rm -rf "$dir/venv"
-  fi
-done
+if [ -f "$dir/environment.yml" ]; then
+  conda deactivate
+  conda remove -y -n $(basename "$dir") --all
+elif [ -f "$dir/requirements.txt" ]; then
+  deactivate
+  rm -rf "$dir/venv"
+fi
+date2=$(date +"%s")
+DIFF=$(($date2-$date1))
+echo "Duration in $dir: $(($DIFF / 3600 )) hours $((($DIFF % 3600) / 60)) minutes $(($DIFF % 60)) seconds"
+# done
