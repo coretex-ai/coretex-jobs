79ca06e72168df0c141d3b7cfa577c89d3d8d741
Dusko Vesic <duskovesic13@gmail.com>
2024-07-24 11:39:08 +0200

    Extract bodies of if/else into train and validate functions

--- tasks/image-quality-predictor/main.py
+++ tasks/image-quality-predictor/main.py
@@ -1,6 +1,6 @@
 import logging
 
-from coretex import currentTaskRun, Artifact, folder_manager, Model
+from coretex import currentTaskRun, Artifact, folder_manager, Model, TaskRun, ImageSample
 from torch.utils.data import DataLoader
 
 import torch
@@ -32,6 +32,86 @@ def fetchArtifacts(validationArtifacts: list[int]) -> list[Artifact]:
     return artifacts
 
 
+def train(taskRun: TaskRun, dataset: list[tuple[ImageSample, float]], transform: transforms.Compose) -> None:
+    if taskRun.parameters["epochs"] is None:
+        raise RuntimeError("The number of epochs for training the model is not defined")
+
+    if taskRun.parameters["validationPct"] is None:
+        raise RuntimeError("validationSplit parameter is not defined")
+
+    epochs: int = taskRun.parameters["epochs"]
+    batchSize: int = taskRun.parameters["batchSize"]
+
+    trainData, validData = split(taskRun.parameters["validationPct"], dataset)
+
+    # Assuming you have your dataset loaded and split into train and test sets
+    trainDataset = ImageQualityDataset(trainData, transform)
+    trainLoader = DataLoader(trainDataset, batchSize, shuffle = True)
+
+    validDataset = ImageQualityDataset(validData, transform)
+    validLoader = DataLoader(validDataset, batchSize)
+
+    # Check if we can train on GPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if torch.cuda.is_available():
+        logging.info(">> [ImageQuality] Using GPU for training")
+    else:
+        logging.info(">> [ImageQuality] Using CPU for training")
+
+    # Initialize the model, loss function, and optimizer
+    model = CNNModel()
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters())
+
+    # Move to the target device (GPU or CPU) for training
+    model.to(device)
+    criterion.to(device)
+
+    modelPath = folder_manager.createTempFolder("model")
+    training.run(taskRun, trainLoader, validLoader, model, optimizer, criterion, epochs, modelPath, device)
+
+    Model.saveModelDescriptor(modelPath, {
+        "taskRunId": taskRun.id,
+        "modelName": taskRun.name,
+        "spaceName": taskRun.projectName,
+        "projectName": taskRun.taskName,
+        "epochs": epochs,
+        "batchSize": batchSize,
+        "imageSize": taskRun.parameters["imageSize"]
+    })
+
+    # Calculate model accuracy
+    logging.info(">> [ImageQuality] Validating model...")
+    sampleResultsCsvPath, accuracy = validation.run(modelPath / "best.pt", trainData + validData, transform)
+    logging.info(f">> [ImageQuality] Model accuracy: {accuracy:.2f}%")
+
+    if taskRun.createArtifact(sampleResultsCsvPath, sampleResultsCsvPath.name) is None:
+        logging.error(f">> [ImageQuality] Failed to create artifact \"{sampleResultsCsvPath.name}\"")
+
+    logging.info(">> [ImageQuality] Uploading model...")
+    ctxModel = Model.createModel(taskRun.generateEntityName(), taskRun.projectId, accuracy)
+    ctxModel.upload(modelPath)
+
+    taskRun.submitOutput("model", ctxModel)
+
+
+def validate(taskRun: TaskRun, dataset: list[tuple[ImageSample, float]], transform: transforms.Compose) -> None:
+    if taskRun.parameters["trainedModel"] is None:
+        raise RuntimeError("Model id used for image quality prediction that needs validation is not valid")
+
+    modelVal: Model = taskRun.parameters["trainedModel"]
+    modelVal.download()
+
+    # Calculate model accuracy
+    logging.info(">> [ImageQuality] Validating model...")
+    sampleResultsCsvPath, accuracy = validation.run(modelVal.path / "best.pt", dataset, transform)
+    logging.info(f">> [ImageQuality] Model accuracy: {accuracy:.2f}%")
+
+    if taskRun.createArtifact(sampleResultsCsvPath, sampleResultsCsvPath.name) is None:
+        logging.error(f">> [ImageQuality] Failed to create artifact \"{sampleResultsCsvPath.name}\"")
+
+
 def main() -> None:
     taskRun = currentTaskRun()
     artifacts = fetchArtifacts(taskRun.parameters["validationArtifacts"])
@@ -48,85 +128,10 @@ def main() -> None:
         transforms.ToTensor()
     ])
 
-    if not taskRun.parameters["validation"]:
-        # training model
-        if taskRun.parameters["epochs"] is None:
-            raise RuntimeError("The number of epochs for training the model is not defined")
-
-        if taskRun.parameters["validationPct"] is None:
-            raise RuntimeError("validationSplit parameter is not defined")
-
-        epochs: int = taskRun.parameters["epochs"]
-        batchSize: int = taskRun.parameters["batchSize"]
-
-        trainData, validData = split(taskRun.parameters["validationPct"], dataset)
-
-        # Assuming you have your dataset loaded and split into train and test sets
-        trainDataset = ImageQualityDataset(trainData, transform)
-        trainLoader = DataLoader(trainDataset, batchSize, shuffle = True)
-
-        validDataset = ImageQualityDataset(validData, transform)
-        validLoader = DataLoader(validDataset, batchSize)
-
-        # Check if we can train on GPU
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        if torch.cuda.is_available():
-            logging.info(">> [ImageQuality] Using GPU for training")
-        else:
-            logging.info(">> [ImageQuality] Using CPU for training")
-
-        # Initialize the model, loss function, and optimizer
-        model = CNNModel()
-        criterion = nn.MSELoss()
-        optimizer = optim.Adam(model.parameters())
-
-        # Move to the target device (GPU or CPU) for training
-        model.to(device)
-        criterion.to(device)
-
-        modelPath = folder_manager.createTempFolder("model")
-        training.run(taskRun, trainLoader, validLoader, model, optimizer, criterion, epochs, modelPath, device)
-
-        Model.saveModelDescriptor(modelPath, {
-            "taskRunId": taskRun.id,
-            "modelName": taskRun.name,
-            "spaceName": taskRun.projectName,
-            "projectName": taskRun.taskName,
-            "epochs": epochs,
-            "batchSize": batchSize,
-            "imageSize": imageSize
-        })
-
-        # Calculate model accuracy
-        logging.info(">> [ImageQuality] Validating model...")
-        sampleResultsCsvPath, accuracy = validation.run(modelPath / "best.pt", trainData + validData, transform)
-        logging.info(f">> [ImageQuality] Model accuracy: {accuracy:.2f}%")
-
-        if taskRun.createArtifact(sampleResultsCsvPath, sampleResultsCsvPath.name) is None:
-            logging.error(f">> [ImageQuality] Failed to create artifact \"{sampleResultsCsvPath.name}\"")
-
-        logging.info(">> [ImageQuality] Uploading model...")
-        ctxModel = Model.createModel(taskRun.generateEntityName(), taskRun.projectId, accuracy)
-        ctxModel.upload(modelPath)
-
-        taskRun.submitOutput("model", ctxModel)
-
+    if taskRun.parameters["validation"]:
+        validate(taskRun, dataset, transform)
     else:
-        # validation of the provided model
-        if taskRun.parameters["trainedModel"] is None:
-            raise RuntimeError("Model id used for image quality prediction that needs validation is not valid")
-
-        modelVal: Model = taskRun.parameters["trainedModel"]
-        modelVal.download()
-
-        # Calculate model accuracy
-        logging.info(">> [ImageQuality] Validating model...")
-        sampleResultsCsvPath, accuracy = validation.run(modelVal.path / "best.pt", dataset, transform)
-        logging.info(f">> [ImageQuality] Model accuracy: {accuracy:.2f}%")
-
-        if taskRun.createArtifact(sampleResultsCsvPath, sampleResultsCsvPath.name) is None:
-            logging.error(f">> [ImageQuality] Failed to create artifact \"{sampleResultsCsvPath.name}\"")
+        train(taskRun, dataset, transform)
 
 
 if __name__ == "__main__":
